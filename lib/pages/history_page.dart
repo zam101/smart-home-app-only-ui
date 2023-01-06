0922d0131ee37e1e529363181b7015d7ddc029a5
zam101 <z_code10@protonmail.com>
2023-01-06 23:56:18 +0100

    add singleChildScrollView to history page to fix overflow on small screen

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -36,11 +36,11 @@ class _HistoryPageState extends State<HistoryPage> {
               )
             ),
             tabIndex == index ?
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical:6.0),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical:6.0),
               child: Icon(Icons.fiber_manual_record,color: primaryColor,size:14),
             )
-            :SizedBox(height:0)
+            :const SizedBox(height:0)
           ],
         ),
       ),
@@ -56,7 +56,7 @@ class _HistoryPageState extends State<HistoryPage> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children:[
-            SizedBox(
+            const SizedBox(
               height:28
             ),
             Row(
@@ -71,10 +71,10 @@ class _HistoryPageState extends State<HistoryPage> {
                         color:secondaryBgColor,
                         borderRadius: BorderRadius.circular(10)
                       ),
-                      child:Icon(Icons.arrow_back)
+                      child:const Icon(Icons.arrow_back)
                     )
                   ),
-                  Text("Air Conditioning",style:TextStyle(color:primaryTextColor,fontSize: 16,fontWeight: FontWeight.bold)),
+                  const Text("Air Conditioning",style:TextStyle(color:primaryTextColor,fontSize: 16,fontWeight: FontWeight.bold)),
                   InkWell(
                     child:Container(
                       width:38,
@@ -83,13 +83,13 @@ class _HistoryPageState extends State<HistoryPage> {
                         color:secondaryBgColor,
                         borderRadius: BorderRadius.circular(10)
                       ),
-                      child:Icon(Icons.upload_file)
+                      child:const Icon(Icons.upload_file)
                     )
                   ),
                   
                 ]
               ),
-            SizedBox(
+            const SizedBox(
               height:20
             ),
             Container(
@@ -105,120 +105,132 @@ class _HistoryPageState extends State<HistoryPage> {
                 ]
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height:20
             ),
-            Center(
-              child: Stack(
-                children: [
-                  Container(
-                    height:300,
-                    width: 300,
-                    decoration:BoxDecoration(
-                      color:Color(0xFFeef0fe),
-                      borderRadius: BorderRadius.circular(150),
-                      border: Border.all(color:Color(0xFFF3F3F3),width: 20)
-                    ),
-                    child:Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children:[
-                        Text("${tempValue.round()}°",style: TextStyle(color:primaryTextColor,fontSize:22,fontWeight:FontWeight.bold),),
-                        SizedBox(height:8),
-                        Text("Temperature",style:TextStyle(color:secondaryTextColor,fontSize: 14,fontWeight: FontWeight.w500))
-                      ]
-                    )
-                  ),
-                  SleekCircularSlider(
-                    innerWidget: (double value){
-                      return SizedBox(height:0);
-                    },
-                    min:0,
-                    max:50,
-                    initialValue: tempValue,
-                    onChange: (double value) {
-                      setState(() {
-                        tempValue = value;
-                      });
-                    },
-                    appearance: CircularSliderAppearance(
-                      startAngle: 270,
-                      angleRange: 360,
-                      size:300,
-                      customWidths: CustomSliderWidths(
-                        progressBarWidth: 4,
-                        trackWidth: 2,
-                        handlerSize: 12
-                      ),
-                      customColors: CustomSliderColors(
-                        trackColor: Color(0xFFE0E0E0),
-                        progressBarColor: primaryColor,
-                        dotColor: primaryColor
-
-                      )
-                    ),
-                  )
-                ]
-              ),
-            ),
-            SizedBox(
-              height:20
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children:[
-                Column(
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Stack(
+                        children: [
+                          Container(
+                            height:300,
+                            width: 300,
+                            decoration:BoxDecoration(
+                              color:const Color(0xFFeef0fe),
+                              borderRadius: BorderRadius.circular(150),
+                              border: Border.all(color:const Color(0xFFF3F3F3),width: 20)
+                            ),
+                            child:Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children:[
+                                Text("${tempValue.round()}°",style: const TextStyle(color:primaryTextColor,fontSize:22,fontWeight:FontWeight.bold),),
+                                const SizedBox(height:8),
+                                const Text("Temperature",style:TextStyle(color:secondaryTextColor,fontSize: 14,fontWeight: FontWeight.w500))
+                              ]
+                            )
+                          ),
+                          SleekCircularSlider(
+                            innerWidget: (double value){
+                              return const SizedBox(height:0);
+                            },
+                            min:0,
+                            max:50,
+                            initialValue: tempValue,
+                            onChange: (double value) {
+                              setState(() {
+                                tempValue = value;
+                              });
+                            },
+                            appearance: CircularSliderAppearance(
+                              startAngle: 270,
+                              angleRange: 360,
+                              size:300,
+                              customWidths: CustomSliderWidths(
+                                progressBarWidth: 4,
+                                trackWidth: 2,
+                                handlerSize: 12
+                              ),
+                              customColors: CustomSliderColors(
+                                trackColor: const Color(0xFFE0E0E0),
+                                progressBarColor: primaryColor,
+                                dotColor: primaryColor
+
+                              )
+                            ),
+                          )
+                        ]
+                      ),
+                    ),
+                const SizedBox(
+                  height:20
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children:[
-                    Text("Air Conditioning",style: TextStyle(color:primaryTextColor,fontSize: 14,fontWeight: FontWeight.w500),),
-                    SizedBox(height:4),
-                    Text("Cras elit nibh",style: TextStyle(color:secondaryTextColor,fontSize: 14,fontWeight: FontWeight.w400),)
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children:const [
+                        Text("Air Conditioning",style: TextStyle(color:primaryTextColor,fontSize: 14,fontWeight: FontWeight.w500),),
+                        SizedBox(height:4),
+                        Text("Cras elit nibh",style: TextStyle(color:secondaryTextColor,fontSize: 14,fontWeight: FontWeight.w400),)
+                      ]
+                    ),
+                    Switch(
+                      splashRadius: 0,
+                      inactiveTrackColor: const Color(0xFFD1D1D1),
+                      activeColor:activeColor,
+                      value:airCond,
+                      onChanged:(bool newValue){
+                        setState(() {
+                          airCond = newValue;
+                        });
+                      }
+                    ),
+
                   ]
                 ),
-                Switch(
-                  splashRadius: 0,
-                  inactiveTrackColor: Color(0xFFD1D1D1),
-                  activeColor:activeColor,
-                  value:airCond,
-                  onChanged:(bool newValue){
-                    setState(() {
-                      airCond = newValue;
-                    });
-                  }
+                const SizedBox(
+                  height:20
                 ),
+                const Text(
+                  "Morbi sed risus magna. Donec blandit, erat air and pharetra tincidunt convallis, neque nisi molestie o metus, quis tincidunt massa urna ut ex. Donec two justo dolor, molestie ut aliquam cursus, sodales an lacinia mi. Sed quis magna ut est accum bedroom.",
+                  style:TextStyle(
+                    color:secondaryTextColor,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400
+                    )
+                  ),
+                const SizedBox(height:20),
+                Container(
+                  height:48,
+                  width:double.infinity,
+                  child: ElevatedButton(
+                    style:ElevatedButton.styleFrom(
+                      elevation: 3,
+                      primary:primaryColor,
+                      minimumSize: const Size(160,48),
+                      shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))
 
-              ]
-            ),
-            SizedBox(
-              height:20
-            ),
-            Text(
-              "Morbi sed risus magna. Donec blandit, erat air and pharetra tincidunt convallis, neque nisi molestie o metus, quis tincidunt massa urna ut ex. Donec two justo dolor, molestie ut aliquam cursus, sodales an lacinia mi. Sed quis magna ut est accum bedroom.",
-              style:TextStyle(
-                color:secondaryTextColor,
-                fontSize: 14,
-                fontWeight: FontWeight.w400
-                )
+                    ),
+                    child:const Text("Save Temperature",style:TextStyle(color:Colors.white,fontWeight: FontWeight.bold,fontSize: 14 )),
+                    onPressed: (){},
+                    ),
+                ),
+                const SizedBox(height:20)
+
+                  ],
+                ),
               ),
-            SizedBox(height:20),
-            Container(
-              height:48,
-              width:double.infinity,
-              child: ElevatedButton(
-                style:ElevatedButton.styleFrom(
-                  elevation: 3,
-                  primary:primaryColor,
-                  minimumSize: const Size(160,48),
-                  shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))
-
-                ),
-                child:Text("Save Temperature",style:TextStyle(color:Colors.white,fontWeight: FontWeight.bold,fontSize: 14 )),
-                onPressed: (){},
-                ),
-            )
-            ]
+            ),
+          ]
         ),
       ), 
     );
